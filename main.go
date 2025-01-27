8536aede07b90a934cad78ef9efd4438d8df9fc6
Hernawan Septiansyah <hernawanseptiansyah@gmail.com>
2025-01-27 18:19:37 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// gin.SetMode(gin.ReleaseMode) //optional to not get warning
+	// route.SetTrustedProxies([]string{"192.168.1.2"}) //to trust only a specific value
+	route := gin.Default()
+	route.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := route.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+
+}
---
+++ models/badan_usaha.go
@@ -0,0 +1,39 @@
+package models
+
+import "gorm.io/gorm"
+
+type BadanUsaha struct {
+	IdBadanUsaha uint `gorm:"primary key;autoIncrement" json:"id_badan_usaha"`
+	IdJenisBadanUsaha uint `json:"id_jenis_badan_usah"`
+	IdJabatan uint `json:"id_jabatan"`
+	NIB string `json:"nib"`
+	NamaBadanUsaha string `json:"nama_badan_usaha"`
+	NoTelp string `json:"no_telp"`
+	Email string `json:"email"`
+	Fax string `json:"fax"`
+	Alamat string `json:"alamat"`
+	Website string `json:"website"`
+	SIUP string `json:"siup"`
+	NpwpBadanUsaha string `json:"npwp_badan_usaha"`
+	KodeNegara string `json:"kode_negara"`
+	BidangBadanUsaha string `json:"bidang_badan_usaha"`
+	KodeBadanUsaha string `json:"kode_badan_usaha"`
+	KodeDesa string `json:"kode_desas"`
+	RT string `json:"rt"`
+	RW string `json:"rw"`
+	IdNegara string `json:"id_negara"`
+	IdOss string `json:"id_oss"`
+	JenisPerseroan string `json:"jenis_perseroan"`
+	JenisPelakuUsaha string `json:"jenis_pelaku_usaha"`
+	JenisKawasanUsaha string `json:"jenis_kawasan_usaha"`
+	TglPengajuanNib string `json:"tgl_pengajuan_nib"`
+	TglPenerbitanNib string `json:"tgl_penerbitan_nib"`
+	TglPerubahanNib string `json:"tgl_perubahan_nib"`
+	StatusBadanHukum string `json:"status_badan_hukum"`
+	Type string `json:"type"`
+}
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&BadanUsaha{})
+	return err
+}
