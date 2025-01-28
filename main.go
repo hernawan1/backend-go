05293da496b4a08d27f21bc4d183aed8b6b92928
Hernawan Septiansyah <hernawanseptiansyah@gmail.com>
2025-01-28 15:53:40 +0700

    add : migrate, get, and create

--- main.go
+++ main.go
@@ -1,24 +1,102 @@
 package main
 
 import (
+	"backend-go/models"
+	"backend-go/storage"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
-func main() {
-	// gin.SetMode(gin.ReleaseMode) //optional to not get warning
-	// route.SetTrustedProxies([]string{"192.168.1.2"}) //to trust only a specific value
-	route := gin.Default()
-	route.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+type Repository struct {
+	DB *gorm.DB
+}
 
-	err := route.Run(":8080")
+type BadanUsaha struct {
+	NamaBadanUsaha string `json:"nama_badan_usaha"`
+	NoTelp         string `json:"no_telp"`
+	Email          string `json:"email"`
+}
+
+func (r *Repository) CreateBadanUsaha(context *fiber.Ctx) error {
+	book := BadanUsaha{}
+
+	err := context.BodyParser(&book)
+
 	if err != nil {
-		panic(err)
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
 	}
+
+	err = r.DB.Create(&book).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create badan usaha"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "badan usaha has been added"})
+	return nil
+}
+
+func (r *Repository) GetBadanUsaha(context *fiber.Ctx) error {
+	badanUsahaModels := &[]models.BadanUsaha{}
+
+	err := r.DB.Find(badanUsahaModels).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get badan usaha"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "badan usaha fetched successfully",
+		"data":    badanUsahaModels,
+	})
+	return nil
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_badan_usaha", r.CreateBadanUsaha)
+	api.Get("/badan_usaha", r.GetBadanUsaha)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
+	db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+	err = models.MigrateBooks(db)
+	if err != nil {
+		log.Fatal("could not migrate db")
+	}
+
+	r := Repository{
+		DB: db,
+	}
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
 
 }
--- storage/postgres.go
+++ storage/postgres.go
@@ -2,7 +2,7 @@ package storage
 
 import (
 	"fmt"
-
+	
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
